f84a65afb3798c474e1b26010fe5538ad8c1bfcb
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-03-27 06:31:36 +1100

    Correcting assembly information.

--- ATZ.Commands.Tests/Properties/AssemblyInfo.cs
+++ ATZ.Commands.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Commands.Tests")]
+[assembly: AssemblyTitle("ATZ.Commands.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Commands.Tests")]
+[assembly: AssemblyCompany("Attila Tamas Zimler")]
+[assembly: AssemblyProduct("ATZ.Commands.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- ATZ.Commands/Properties/AssemblyInfo.cs
+++ ATZ.Commands/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Commands")]
+[assembly: AssemblyTitle("ATZ.Commands")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Commands")]
+[assembly: AssemblyCompany("Attila Tamas Zimler")]
+[assembly: AssemblyProduct("ATZ.Commands")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
