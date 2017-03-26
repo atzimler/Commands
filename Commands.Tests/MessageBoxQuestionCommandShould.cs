e20f260499fb8e2f211a7005aa2d6b85ece03d27
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-03-27 04:57:08 +1100

    Adding ATZ.DependencyInjection.System.Windows nuget package.

---
+++ Commands.Tests/MessageBoxQuestionCommandShould.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+// ReSharper disable PossibleNullReferenceException => Moq and Fluent Assertions don't contain JetBrains annotations and as a result they are
+// triggering the null reference checker. However, this is a test code, so even if there is a null reference, it is not the end of the world.
+// The situation of having a null non-fatal null reference exception allows us to switch off the check on file level, because even if the
+// reason for such exception is something else, it will not cause product quality issues.
+
+namespace Commands.Tests
+{
+    // TODO: Add and test various IMessageBox calls. Interface should include possibility to specify user approval values.
+
+    [TestFixture]
+    public class MessageBoxQuestionCommandShould
+    {
+        [Test]
+        public void BeExecutableByDefault()
+        {
+            var cmd = new MessageBoxQuestionCommand();
+            cmd.CanExecute(null).Should().BeTrue();
+        }
+
+        [Test]
+        public void ProperlyExecutesMessageBoxText()
+        {
+            
+        }
+    }
+}
---
+++ Commands/MessageBoxQuestionCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Commands
+{
+    /// <summary>
+    /// Command to ask a user a question on the View level of MVVM code structure.
+    /// </summary>
+    public class MessageBoxQuestionCommand : ICommand
+    {
+        /// <summary>
+        /// Event raised when CanExecute(parameter) value is possible changed. In the current implementation, the CanExecute value is
+        /// bound to user decision and as a result the parameter is ignored.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+
+        /// <summary>
+        /// Queries if the execution of the command is allowed.
+        /// </summary>
+        /// <param name="parameter">Ignored, present for ICommand interface compatibility.</param>
+        /// <returns>True if the command can be executed either because the user has not been asked the question yet, or because the user approved the question.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Ask the user the question through a message dialog.
+        /// </summary>
+        /// <param name="parameter">Ignored, present for ICommand interface compatibility.</param>
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reset the CanExecute flag of the command, so the question can be asked again.
+        /// </summary>
+        public void ResetCanExecute()
+        {
+            
+        }
+    }
+}
