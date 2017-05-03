6dea158cb606c460b6ef5906254844563c209a1b
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-05-04 06:53:52 +1000

    Version 1.2.0, Addition of IfCommand.

---
+++ ATZ.Commands.Tests/IfCommandShould.cs
@@ -0,0 +1,110 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Windows.Input;
+
+namespace ATZ.Commands.Tests
+{
+    [TestFixture]
+    public class IfCommandShould
+    {
+        [Test]
+        public void ExecuteIfBranchIfConditionIsTrue()
+        {
+            var cmd = new Mock<ICommand>();
+            cmd.Setup(c => c.Execute(null));
+
+            var ifCommand = new IfCommand(o => true, cmd.Object);
+
+            ifCommand.Execute(null);
+            cmd.VerifyAll();
+        }
+
+        [Test]
+        public void NotExecuteIfBranchIfConditionIsFalse()
+        {
+            var cmd = new Mock<ICommand>(MockBehavior.Strict);
+
+            var ifCommand = new IfCommand(o => false, cmd.Object);
+
+            ifCommand.Execute(null);
+        }
+
+        [Test]
+        public void ThrowArgumentNullExceptionIfConditionIsNull()
+        {
+            // ReSharper disable once ObjectCreationAsStatement => We are testing the constructor.
+            var ex = Assert.Throws<ArgumentNullException>(() => new IfCommand(null, null));
+            Assert.IsNotNull(ex);
+            Assert.AreEqual("condition", ex.ParamName);
+        }
+
+        [Test]
+        public void NotCauseNullReferenceExceptionIfIfBranchIsNull()
+        {
+            var ifCommand = new IfCommand(o => true, null);
+            Assert.DoesNotThrow(() => ifCommand.Execute(null));
+        }
+
+        [Test]
+        public void ReturnTrueForCanExecuteSinceExecutionWillBeDecidedFromTheCondition()
+        {
+            var ifCommand = new IfCommand(o => true, null);
+            Assert.IsTrue(ifCommand.CanExecute(null));
+        }
+
+        [Test]
+        public void PassExecutionParameterToTheCondition()
+        {
+            var p = new object();
+            var ifCommand = new IfCommand(o =>
+            {
+                Assert.AreSame(p, o);
+                return true;
+            }, null);
+            Assert.DoesNotThrow(() => ifCommand.Execute(p));
+        }
+
+        [Test]
+        public void PassParameterToTheExecutedCommand()
+        {
+            var p = new object();
+            var cmd = new Mock<ICommand>(MockBehavior.Strict);
+            cmd.Setup(c => c.Execute(p));
+
+            var ifCommand = new IfCommand(o => true, cmd.Object);
+            ifCommand.Execute(p);
+        }
+
+        [Test]
+        public void ExecuteElseBranchIfConditionIsFalse()
+        {
+            var tcmd = new Mock<ICommand>(MockBehavior.Strict);
+            var fcmd = new Mock<ICommand>(MockBehavior.Strict);
+            fcmd.Setup(c => c.Execute(null));
+
+            var ifCommand = new IfCommand(o => false, tcmd.Object, fcmd.Object);
+            ifCommand.Execute(null);
+
+            fcmd.VerifyAll();
+        }
+
+        [Test]
+        public void NotThrowExceptionIfElseCommandIsNull()
+        {
+            var ifCommand = new IfCommand(o => false, null);
+            Assert.DoesNotThrow(() => ifCommand.Execute(null));
+        }
+
+        [Test]
+        public void PassParameterToTheExecutedElseBranchCommand()
+        {
+            var p = new object();
+            var cmd = new Mock<ICommand>(MockBehavior.Strict);
+            cmd.Setup(c => c.Execute(p));
+
+            var ifCommand = new IfCommand(o => false, null, cmd.Object);
+            ifCommand.Execute(p);
+        }
+    }
+}
---
+++ ATZ.Commands/IfCommand.cs
@@ -0,0 +1,65 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Windows.Input;
+
+namespace ATZ.Commands
+{
+    /// <summary>
+    /// Command to evaluate a function during execution and deciding on which branch of commands to execute, if any.
+    /// </summary>
+    public class IfCommand : ICommand
+    {
+        [NotNull]
+        private readonly Func<object, bool> _condition;
+        private readonly ICommand _ifBranch;
+        private readonly ICommand _elseBranch;
+
+        /// <summary>
+        /// Never raised as the CanExecute always returns true, to allow evaluation of the function and decision on the commands to execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="condition">The condition to evaluate during the execution. The object passed to the condition is the parameter of the execution.</param>
+        /// <param name="ifBranch">Command to execute if the condition is true. If null, no command will be executed on true condition.</param>
+        /// <param name="elseBranch">Command to execute if the condition is false. If null, no command will be executed on false condition.</param>
+        public IfCommand(Func<object, bool> condition, ICommand ifBranch, ICommand elseBranch = null)
+        {
+            _condition = condition ?? throw new ArgumentNullException(nameof(condition));
+            _ifBranch = ifBranch;
+            _elseBranch = elseBranch;
+        }
+
+        /// <summary>
+        /// Always returns true, to allow evaluation of the condition during execution of the command.
+        /// </summary>
+        /// <param name="parameter">Ignored as the function always returns true.</param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Execute the command.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter to be passed to the executed command and the condition to be evaluated.
+        /// <see cref="IfCommand(Func&lt;object, bool&gt;, ICommand, ICommand)"/>
+        /// </param>
+        public void Execute(object parameter)
+        {
+            if (_condition(parameter))
+            {
+                _ifBranch?.Execute(parameter);
+            }
+            else
+            {
+                _elseBranch?.Execute(parameter);
+            }
+        }
+
+    }
+}
